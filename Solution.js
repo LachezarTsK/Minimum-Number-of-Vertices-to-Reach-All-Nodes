c70181370d80193a9e301846a71e17a6f24d699d
Lachezar Kutsarov <lkutsarov@gmail.com>
2022-04-06 07:27:30 +0200

    Update Solution.js

--- Solution.js
+++ Solution.js
@@ -7,13 +7,13 @@
 var findSmallestSetOfVertices = function (totalNodes, edges) {
     const nodeHasIncomingEdges = new Array(totalNodes).fill(false);
     for (let edge of edges) {
-        nodeHasIncomingEdges[edge.get(1)] = true;
+        nodeHasIncomingEdges[edge[1]] = true;
     }
 
     const smallestSetOfVertices = [];
     for (let i = 0; i < totalNodes; i++) {
         if (nodeHasIncomingEdges[i] === false) {
-            smallestSetOfVertices.add(i);
+            smallestSetOfVertices.push(i);
         }
     }
     return smallestSetOfVertices;
